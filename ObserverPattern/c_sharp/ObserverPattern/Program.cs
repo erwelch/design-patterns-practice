ad1fa5e24a9595080023cca0d275707ee996187e
Erin Welch <erwelch@Erins-MBP.guest.corp.microsoft.com>
2019-08-21 12:00:46 -0700

    Start of new pattern.

---
+++ ObserverPattern/c_sharp/ObserverPattern/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface Subject {
+        void registerObserver(Observer o);
+        void removeObserver(Observer o);
+
+        void notifyObservers(); 
+    }
+
+    public interface Observer {
+        void update();
+    }
+}
